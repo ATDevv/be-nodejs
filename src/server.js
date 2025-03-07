14e2e62887c13542862d1598888100a4b9fc524d
Tuấngg Anhhh <macofanh@Tuongg-Anhh.local>
2025-03-07 15:48:03 +0700

    dynamic content express

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const path = require('path') //Common js
+
 const app = express()
 const port = 8080
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.render('sample.ejs')
 })
 
 app.listen(port, () => {
