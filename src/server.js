5fdfb936c51fe58538a65779c42dd08a8c24f9ae
Tuấngg Anhhh <macofanh@Tuongg-Anhh.local>
2025-03-08 21:26:31 +0700

    .env node

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const path = require('path') //Common js
+require('dotenv').config()
 
 const app = express()
-const port = 8080
+const port = process.env.PORT
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -12,7 +13,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.render('sample.ejs')
+    res.render('sample1.ejs')
+})
+
+app.get('/product', (req, res) => {
+    res.render('sample2.ejs')
 })
 
 app.listen(port, () => {
