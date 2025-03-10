b9fb83bca4939ebe1466118a037ee8a2060e61cb
Tuấngg Anhhh <macofanh@Tuongg-Anhh.local>
2025-03-10 09:44:13 +0700

    reorganize folder

--- src/server.js
+++ src/server.js
@@ -1,21 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path') //Common js
-require('dotenv').config()
+const configViewEngine = require('./config/viewEngine')
+const webRoutes = require("./routes/web")
 
 const app = express()
 const port = process.env.PORT
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(path.join(__dirname, 'public')))
+//config view engine
+configViewEngine(app)
 
-app.get('/', (req, res) => {
-    res.send('Hello World! 123')
-})
-
-app.get('/about', (req, res) => {
-    res.render('sample.ejs')
-})
+//route
+app.use('/', webRoutes)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
