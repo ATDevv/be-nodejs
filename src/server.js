95a53049e4d828640a68c988896d8be58ebce65e
Tuấngg Anhhh <macofanh@Tuongg-Anhh.local>
2025-03-31 16:23:27 +0700

    connect mongo with mongoose

--- src/server.js
+++ src/server.js
@@ -15,9 +15,17 @@ app.use(express.urlencoded({ extended: false }))
 //config view engine
 configViewEngine(app)
 
+//test connection
+;(async () => {
+    try {
+        await connection()
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+    } catch (error) {
+        console.log('>>> Error connect to db ', error)
+    }
+})()
+
 //route
 app.use('/', webRoutes)
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
