3c5ce3f2c0c33d7c6ca705548b90cbd9017a3a2e
Tuấngg Anhhh <macofanh@Tuongg-Anhh.local>
2025-04-07 18:08:07 +0700

    create user with post resful

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -3,10 +3,26 @@ const User = require('../models/user')
 const getUserAPI = async (req, res) => {
     const users = await User.find({})
     return res.status(200).json({
-        data: users
+        data: users,
+    })
+}
+
+const postUserAPI = async (req, res) => {
+    let { email, name, city } = req.body
+
+    const user = await User.create({
+        email: email,
+        name: name,
+        city: city,
+    })
+
+    return res.status(200).json({
+        EC: 0,
+        data: user,
     })
 }
 
 module.exports = {
-    getUserAPI
+    getUserAPI,
+    postUserAPI,
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const routerAPI = express.Router()
-const { getUserAPI } = require('../controllers/apiController')
+const { getUserAPI, postUserAPI } = require('../controllers/apiController')
 
 routerAPI.get('/', (req, res) => {
     res.send('Hi api')
 })
 
 routerAPI.get('/users', getUserAPI)
+routerAPI.post('/users', postUserAPI)
 
 module.exports = routerAPI
